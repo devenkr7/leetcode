51317871c75662feb9bdac5e96e11af1597fa920
Devender kumar <devendermtf@gmail.com>
2022-03-15 01:19:22 +0530

    Sliding window - MaxSumSubArrayOfSizeK and FirstNegNumInWindowSizeK

---
+++ src/Conceptual/SlidingWindow/FirstNegNumInWindowSizeK.java
@@ -0,0 +1,38 @@
+package Conceptual.SlidingWindow;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+//https://practice.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1/
+public class FirstNegNumInWindowSizeK {
+
+    public static void main(String[] args) {
+        int[] arr = {12,-1, -7, 8, -15, 30, 16, 28};
+        int k = 3;
+        ArrayList<Integer> result = firstNegNumInWindowSizeK(arr, k);
+        System.out.println(result);
+    }
+
+    public static ArrayList<Integer> firstNegNumInWindowSizeK(int[] arr, int k){
+        ArrayList<Integer> res = new ArrayList<>();
+        Queue<Integer> negQ = new LinkedList<>();
+        int i=0, j=0;
+        while(j< arr.length){
+            if(arr[j] < 0) {
+                negQ.add(arr[j]);
+            }
+            if(j-i+1 < k){
+                j++;//reach window size first
+            } else if(j-i+1 == k){
+                res.add(negQ.size() != 0 ? negQ.peek() : 0); //first element else 0
+                if(negQ.size() != 0 && negQ.peek() == arr[i]){ // checking for arr[i] since we will be incrementing i
+                    negQ.remove(arr[i]);
+                }
+                i++; j++;
+            }
+        }
+
+        return res;
+    }
+}
---
+++ src/Conceptual/SlidingWindow/MaxSumSubArrayOfSizeK.java
@@ -0,0 +1,27 @@
+package Conceptual.SlidingWindow;
+
+//https://practice.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1/
+public class MaxSumSubArrayOfSizeK {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,7,3,5};
+        int k = 3;
+        System.out.println(maxSumSubArraySizeK(arr, k));
+    }
+
+    private static int maxSumSubArraySizeK(int[] arr, int k) {
+        int i=0, j=0, sum=0, maxVar = Integer.MIN_VALUE;
+        // window size = j-i+1
+        while(j< arr.length) {
+            sum = sum + arr[j];
+            if(j-i+1 < k) {
+                j++; // to reach window size first
+            } else if(j-i+1 == k){
+                maxVar = Math.max(maxVar, sum);
+                sum = sum - arr[i]; //removing first element of window
+                i++; j++;
+            }
+        }
+        return maxVar;
+    }
+}
