f6a98e31c5fc9f9852fba394a48e784cdec28a86
Devender kumar <devendermtf@gmail.com>
2022-03-14 23:58:56 +0530

    Started sliding window algos

---
+++ src/Conceptual/SlidingWindow/MaxSumSubarrayOfSizeK.java
@@ -0,0 +1,14 @@
+package Conceptual.SlidingWindow;
+
+public class MaxSumSubarrayOfSizeK {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,7,3,5};
+        int k = 3;
+
+        int i=0, j=0, sum=0, max = Integer.MAX_VALUE;
+        while(j<arr.length) {
+            sum = sum+arr[j];
+        }
+    }
+}
